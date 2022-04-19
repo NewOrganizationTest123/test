96f7bdc3b28feaf1e8f7ac604e5da51b546bf143
Silvio Pettauer <sipettauer@edu.aau.at>
2022-04-19 19:13:21 +0200

    feat(menu):Menu linking to connected Sites

--- app/src/main/java/com/example/myapplication/MenuActivity.java
+++ app/src/main/java/com/example/myapplication/MenuActivity.java
@@ -5,12 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button spielen;
+    Button einstellungen;
+    Button anleitung;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+
+        spielen = (Button) findViewById(R.id.spielbutton);
+        einstellungen = (Button) findViewById(R.id.einstellungbutton);
+        anleitung = (Button) findViewById(R.id.anleitungbutton);
+        spielen.setOnClickListener(this);
+        einstellungen.setOnClickListener(this);
+        anleitung.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.spielbutton:
+                startActivity(new Intent(MenuActivity.this,SpielnameActivity.class));
+                break;
+            case R.id.einstellungbutton:
+                startActivity(new Intent(MenuActivity.this,EinstellungenActivity.class));
+                break;
+            case R.id.anleitungbutton:
+                startActivity(new Intent(MenuActivity.this,AnleitungActivity.class));
+                break;
+        }
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
