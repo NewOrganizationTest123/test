e8da10b376da0521a8edec8014d3ef2061035677
Laura Randl <gollumeye@outlook.de>
2022-04-25 00:45:10 +0200

    test: added some test cases for Stich.java

--- server/src/test/java/com/github/wizard/StichTests.java
+++ server/src/test/java/com/github/wizard/StichTests.java
@@ -4,12 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.*;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-
-@ExtendWith(MockitoExtension.class)
 public class StichTests {
     Stich stich;
     Card card1;
