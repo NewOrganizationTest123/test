8bc1e9b603e8cf432a9b6e85f674e04f13b1ffbd
Gregor <gregor2@outlook.at>
2022-05-31 22:31:55 +0200

    helping Silvio with ScoreboardFragment.java

--- app/src/main/java/com/github/wizard/ScoreboardFragment.java
+++ app/src/main/java/com/github/wizard/ScoreboardFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,6 +16,13 @@ public class ScoreboardFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_scoreboard, container, false);
+
+        TextView name=view.findViewById(R.id.Namestext);//todo @silvio this is how to access something in the layout
+        for (String player : GamePlayActivity.players) {
+            name.setText(player);//todo @silvio this is how you loop over the players array to fill your view...
+        }
+
         return view;
     }
+
 }
