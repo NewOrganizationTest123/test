c9e186bc15757d20fd3f29485f98d4bc9eb03e02
Silvio Pettauer <sipettauer@edu.aau.at>
2022-05-31 17:01:55 +0200

    feat: added class from server branch

---
+++ app/src/main/java/com/github/wizard/ClientPlayer.java
@@ -0,0 +1,13 @@
+package com.github.wizard;
+
+public class ClientPlayer {
+    public String id;
+    public String name;
+    public String points;
+
+    public ClientPlayer(String id, String name, String points) {
+        this.id = id;
+        this.name = name;
+        this.points = points;
+    }
+}
