f7bb624d85caf0a4cf411a8414033aed522ac593
Silvio Pettauer <sipettauer@edu.aau.at>
2022-05-31 21:08:20 +0200

    feat: fragment added

---
+++ app/src/main/java/com/github/wizard/ScoreboardFragment.java
@@ -0,0 +1,20 @@
+package com.github.wizard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class ScoreboardFragment extends Fragment {
+
+    View view;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_scoreboard, container, false);
+        return view;
+    }
+}
