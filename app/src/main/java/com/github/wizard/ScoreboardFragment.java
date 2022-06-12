79c062d0085c494aa6f7bf9ec8fa7d068fed58d0
Felix Hilgers <felix_hilgers@posteo.de>
2022-06-12 21:52:45 +0200

    refactor[app/scoreboard]: remove duplication

--- app/src/main/java/com/github/wizard/ScoreboardFragment.java
+++ app/src/main/java/com/github/wizard/ScoreboardFragment.java
@@ -14,62 +14,57 @@ public class ScoreboardFragment extends Fragment {
 
     View view;
 
+    private final int[] playerNameIds = {
+        R.id.Player1Name,
+        R.id.Player2Name,
+        R.id.Player3Name,
+        R.id.Player4Name,
+        R.id.Player5Name,
+        R.id.Player6Name
+    };
+    private final int[] playerPointIds = {
+        R.id.Player1Points,
+        R.id.Player2Points,
+        R.id.Player3Points,
+        R.id.Player4Points,
+        R.id.Player5Points,
+        R.id.Player6Points
+    };
+    private int roundId = R.id.Roundscounter;
+
+    private List<TextView> playerNameViews;
+    private List<TextView> playerPointViews;
+
+    private List<TextView> getViewsFromIds(int[] ids) {
+        List<TextView> views = new ArrayList<>(6);
+        for (int id : ids) {
+            views.add(view.findViewById(id));
+        }
+        return views;
+    }
+
+    private void setupPlayerNameViews() {
+        this.playerNameViews = getViewsFromIds(playerNameIds);
+    }
+
+    private void setupPlayerPointViews() {
+        this.playerPointViews = getViewsFromIds(playerPointIds);
+    }
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_scoreboard, container, false);
 
-        TextView name1 = view.findViewById(R.id.Player1Name);
-        TextView name2 = view.findViewById(R.id.Player2Name);
-        TextView name3 = view.findViewById(R.id.Player3Name);
-        TextView name4 = view.findViewById(R.id.Player4Name);
-        TextView name5 = view.findViewById(R.id.Player5Name);
-        TextView name6 = view.findViewById(R.id.Player6Name);
-        TextView points1 = view.findViewById(R.id.Player1Points);
-        TextView points2 = view.findViewById(R.id.Player2Points);
-        TextView points3 = view.findViewById(R.id.Player3Points);
-        TextView points4 = view.findViewById(R.id.Player4Points);
-        TextView points5 = view.findViewById(R.id.Player5Points);
-        TextView points6 = view.findViewById(R.id.Player6Points);
-        TextView round = view.findViewById(R.id.Roundscounter);
-        List<ClientPlayer> players = new ArrayList<>();
-        players = GamePlayActivity.getPlayers();
+        setupPlayerNameViews();
+        setupPlayerPointViews();
 
-        int i = 0;
-        while (i < players.size()) {
-            switch (i) {
-                case 0:
-                    name1.setText(players.get(0).getName());
-                    points1.setText(players.get(0).getPoints());
-                    i++;
-                    break;
-                case 1:
-                    name2.setText(players.get(1).getName());
-                    points2.setText(players.get(1).getPoints());
-                    i++;
-                    break;
-                case 2:
-                    name3.setText(players.get(2).getName());
-                    points3.setText(players.get(2).getPoints());
-                    i++;
-                    break;
-                case 3:
-                    name4.setText(players.get(3).getName());
-                    points4.setText(players.get(3).getPoints());
-                    i++;
-                    break;
-                case 4:
-                    name5.setText(players.get(4).getName());
-                    points5.setText(players.get(4).getPoints());
-                    i++;
-                    break;
-                case 5:
-                    name6.setText(players.get(5).getName());
-                    points6.setText(players.get(5).getPoints());
-                    i++;
-                    break;
-            }
+        List<ClientPlayer> players = GamePlayActivity.getPlayers();
+
+        for (int i = 0; i < players.size(); i++) {
+            playerNameViews.get(i).setText(players.get(i).getName());
+            playerPointViews.get(i).setText(players.get(i).getPoints());
         }
 
         return view;
@@ -77,47 +72,18 @@ public class ScoreboardFragment extends Fragment {
 
     public void winningplayerhighlighted() {
 
-        TextView name1 = view.findViewById(R.id.Player1Name);
-        TextView name2 = view.findViewById(R.id.Player2Name);
-        TextView name3 = view.findViewById(R.id.Player3Name);
-        TextView name4 = view.findViewById(R.id.Player4Name);
-        TextView name5 = view.findViewById(R.id.Player5Name);
-        TextView name6 = view.findViewById(R.id.Player6Name);
-        TextView points1 = view.findViewById(R.id.Player1Points);
-        TextView points2 = view.findViewById(R.id.Player2Points);
-        TextView points3 = view.findViewById(R.id.Player3Points);
-        TextView points4 = view.findViewById(R.id.Player4Points);
-        TextView points5 = view.findViewById(R.id.Player5Points);
-        TextView points6 = view.findViewById(R.id.Player6Points);
-        TextView round = view.findViewById(R.id.Roundscounter);
+        // ensure that the views are fetched
+        setupPlayerPointViews();
 
         ClientPlayer winner = GamePlayActivity.getPlayers().get(0);
-        for (ClientPlayer p : GamePlayActivity.getPlayers())
+        for (ClientPlayer p : GamePlayActivity.getPlayers()) {
             if (Integer.parseInt(p.getPoints()) > Integer.parseInt(winner.getPoints())) winner = p;
-
-        if (GamePlayActivity.getPlayers().indexOf(winner) == 0) {
-            points1.setBackgroundColor(
-                    ContextCompat.getColor(this.getContext(), android.R.color.holo_green_light));
-
-        } else if (GamePlayActivity.getPlayers().indexOf(winner) == 1) {
-            points2.setBackgroundColor(
-                    ContextCompat.getColor(this.getContext(), android.R.color.holo_green_light));
-
-        } else if (GamePlayActivity.getPlayers().indexOf(winner) == 2) {
-            points3.setBackgroundColor(
-                    ContextCompat.getColor(this.getContext(), android.R.color.holo_green_light));
-
-        } else if (GamePlayActivity.getPlayers().indexOf(winner) == 3) {
-            points4.setBackgroundColor(
-                    ContextCompat.getColor(this.getContext(), android.R.color.holo_green_light));
-
-        } else if (GamePlayActivity.getPlayers().indexOf(winner) == 4) {
-            points5.setBackgroundColor(
-                    ContextCompat.getColor(this.getContext(), android.R.color.holo_green_light));
-
-        } else if (GamePlayActivity.getPlayers().indexOf(winner) == 5) {
-            points6.setBackgroundColor(
-                    ContextCompat.getColor(this.getContext(), android.R.color.holo_green_light));
         }
+
+        int winnerIndex = GamePlayActivity.getPlayers().indexOf(winner);
+        TextView winnerPointView = playerPointViews.get(winnerIndex);
+
+        winnerPointView.setBackgroundColor(
+                ContextCompat.getColor(getContext(), android.R.color.holo_green_light));
     }
 }
