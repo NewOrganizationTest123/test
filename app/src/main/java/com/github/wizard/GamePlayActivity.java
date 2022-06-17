6e1152dda0edd6553347301307b62484ef59077b
Laura Randl <gollumeye@outlook.de>
2022-06-17 12:36:26 +0200

    fix: changed german words on GUI to english

--- app/src/main/java/com/github/wizard/GamePlayActivity.java
+++ app/src/main/java/com/github/wizard/GamePlayActivity.java
@@ -337,7 +337,7 @@ public class GamePlayActivity extends AppCompatActivity {
 
     private void updateEstimateTextview(String estimate) {
         TextView estimateTextView = findViewById(R.id.stiche_estimated);
-        estimateTextView.setText("You wanted to make " + estimate + " Stiche");
+        estimateTextView.setText("You wanted to make " + estimate + " tricks");
     }
 
     private void submitEstimate(String estimate) {
@@ -404,7 +404,7 @@ public class GamePlayActivity extends AppCompatActivity {
 
     public void updateNumberOfStichesTextview() {
         TextView stiche = findViewById(R.id.stiche_made);
-        stiche.setText("You have already made " + numberOfStitchesMade + " Stiche");
+        stiche.setText("You have already made " + numberOfStitchesMade + " tricks");
     }
 
     private class GameActionRunnable implements GrpcRunnableNew {
@@ -473,7 +473,7 @@ public class GamePlayActivity extends AppCompatActivity {
                                                 .setText(
                                                         "You have already made "
                                                                 + stichmade.getTotalstichebyplayer()
-                                                                + " Stiche");
+                                                                + " tricks");
 
                                     } else { // someone else made the stich
                                         Toast.makeText(
@@ -560,7 +560,7 @@ public class GamePlayActivity extends AppCompatActivity {
                                     Toast.makeText(
                                                     activity.getApplication()
                                                             .getApplicationContext(),
-                                                    "trumpf is: " + response.getData(),
+                                                    "trump is: " + response.getData(),
                                                     Toast.LENGTH_SHORT)
                                             .show();
                                 }
@@ -573,7 +573,7 @@ public class GamePlayActivity extends AppCompatActivity {
                                         Activity activity, GameStatus gameStatus) {
                                     // reset stich counter to 0 when new round starts
                                     ((TextView) activity.findViewById(R.id.stiche_made))
-                                            .setText("You have already made 0 Stiche");
+                                            .setText("You have already made 0 tricks");
 
                                     for (GrpcPlayer grpcPlayer : gameStatus.getPlayersList()) {
                                         // update my points
